7a9e68027053a383e21ea0cc6f165abfc0561d3b
alwaysuttam <itsuttammail@gmail.com>
2024-01-22 11:56:21 +0530

    requirements installation done

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md","r", encoding = "utf-8") as f:
+    long_description = f.read()
+
+
+__version__="0.0.0"
+
+REPO_NAME = "text_summarizer"
+AUTHOR_USERNAME = "alwaysuttam"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "itsuttammail@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USERNAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alwaysuttam/textSummarizer",
+    project_urls={
+        "Bug Tracker": "https://github.com/alwaysuttam/textSummarizer/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
